8cf76ecdd35c09141bb7aeb25fc0bca635cfa5d1
pinchukserj <psvnovgorod@gmail.com>
2022-08-04 20:16:35 +0300

    Add generator.py and dataclass Person

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -9,10 +9,10 @@ class TestElements:
         def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
             text_box_page.open()
-            text_box_page.fill_all_fields()
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
             output_name, output_email, output_curr_addr, output_per_addr = text_box_page.check_filled_form()
-            print(output_name)
-            print(output_email)
-            print(output_curr_addr)
-            print(output_per_addr)
-
+            time.sleep(3)
+            assert full_name == output_name, "full name does not match"
+            assert email == output_email, "email does not match"
+            assert current_address == output_curr_addr, "current address does not match"
+            assert permanent_address == output_per_addr, "permanent address does not match"
