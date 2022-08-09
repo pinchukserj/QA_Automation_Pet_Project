d26aed6cf582a16ef88fb3e8f8d82dd52fe4b83a
pinchukserj <psvnovgorod@gmail.com>
2022-08-09 18:10:25 +0300

    Fixed go_to_elements in base_page.py Update elements_test.py Added test checkboxes

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,6 +1,6 @@
 import time
 
-from pages.elements_page import TextBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage
 
 
 class TestElements:
@@ -16,3 +16,13 @@ class TestElements:
             assert email == output_email, "email does not match"
             assert current_address == output_curr_addr, "current address does not match"
             assert permanent_address == output_per_addr, "permanent address does not match"
+
+    class TestCheckBox:
+        def test_checkbox(self, driver):
+            check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox')
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_checkbox = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            assert input_checkbox == output_result, "Input checkboxes and output result does not match"
