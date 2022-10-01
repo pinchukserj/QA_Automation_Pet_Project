f346a92f5ddc28546c1b9f9191b8b1519b4cc446
pinchukserj <psvnovgorod@gmail.com>
2022-10-01 23:42:56 +0300

    Added class: -TestSortablePage
    Added test: -test_sortable

--- pages/base_page.py
+++ pages/base_page.py
@@ -44,7 +44,12 @@ class BasePage:
 
     def action_drag_and_drop_by_offset(self, element, x_coords, y_coords):
         action = ActionChains(self.driver)
-        action.drag_and_drop_by_offset(element, x_coords, y_coords)
+        action.drag_and_drop(element, x_coords, y_coords)
+        action.perform()
+
+    def action_drag_and_drop_to_element(self, what, where):
+        action = ActionChains(self.driver)
+        action.drag_and_drop(what, where)
         action.perform()
 
     def delete_footer(self):
---
+++ tests/interactions_test.py
@@ -0,0 +1,14 @@
+from pages.interactions_page import SortablePage
+
+
+class TestInteractions:
+
+    class TestSortablePage:
+
+        def test_sortable(self, driver):
+            sortable_page = SortablePage(driver, "https://demoqa.com/sortable")
+            sortable_page.open()
+            list_before, list_after = sortable_page.change_list_order()
+            grid_before, grid_after = sortable_page.change_grid_order()
+            assert list_before != list_after, "drag and drop has not been done"
+            assert grid_before != grid_after, "drag and drop has not been done"
