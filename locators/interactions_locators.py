2d313dadbfe7ac0aeef17554dfcd8ba99eda2d8e
pinchukserj <psvnovgorod@gmail.com>
2022-10-17 22:32:51 +0300

    Added class: -TestDraggablePage
    Added tests: -test_simple_drag

--- locators/interactions_locators.py
+++ locators/interactions_locators.py
@@ -50,7 +50,4 @@ class DraggablePageLocators:
     SIMPLE_TAB = (By.CSS_SELECTOR, "a[id='draggableExample-tab-simple']")
     DRAG_ME = (By.CSS_SELECTOR, "div[id='dragBox']")
 
-    # Axis Restricted
-    AXIS_TAB = (By.CSS_SELECTOR, "a[id='draggableExample-tab-axisRestriction']")
-    ONLY_X = (By.CSS_SELECTOR, "div[id='restrictedX']")
-    ONLY_Y = (By.CSS_SELECTOR, "div[id='restrictedY']")
+
